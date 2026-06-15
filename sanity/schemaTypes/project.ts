99e643a7e1a70e1aa3b2f6ed5e9cef8da763ba0c
Johnny Nguyen <johnnynguyen@Johnnys-Mac-mini.local>
2026-06-15 19:42:36 +0700

    fix type error

--- sanity/schemaTypes/project.ts
+++ sanity/schemaTypes/project.ts
@@ -143,9 +143,7 @@ export const projectType = defineType({
       name: 'mapHtml',
       title: 'Bản Đồ Vị Trí (Google Maps Iframe)',
       type: 'text',
-      options: {
-        rows: 3
-      },
+      rows: 3,
       description: 'Dán mã nhúng <iframe> lấy từ Google Maps vào đây. (Hiển thị phía trên)',
     }),
     defineField({
